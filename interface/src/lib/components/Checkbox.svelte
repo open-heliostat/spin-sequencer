40b2c9ef0190c034074c79dd9bf581669bce604e
leokeba <leokeba@gmail.com>
2024-08-21 21:12:27 +0200

    add ui components

---
+++ interface/src/lib/components/Checkbox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let label: string;
+    export let value: boolean;
+    export let onChange = () => {};
+</script>
+
+<label class="label cursor-pointer" for={label}>
+    <span class="mr-4">{label}</span>
+</label>
+<input 
+    type="checkbox"
+    class="toggle toggle-primary"
+    id={label}
+    bind:checked={value}
+    on:change={onChange}
+/>
---
+++ interface/src/lib/components/Slider.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let label: string;
+    export let value: number;
+    export let min = 0;
+    export let max = 1;
+    export let step = 0.01;
+    export let onChange: () => void;
+</script>
+
+<label class="label cursor-pointer" for={label}>
+    <span class="mr-4">{label}</span>
+</label>
+<input 
+    type="range"
+    min={min} 
+    max={max}
+    step={step}
+    class="range range-primary"
+    id={label}
+    bind:value={value}
+    on:change={onChange}
+/>
