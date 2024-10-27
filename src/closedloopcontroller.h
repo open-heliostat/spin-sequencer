fd7e866add949a2677b3c161fefa7d30fdca5363
leokeba <leokeba@gmail.com>
2024-10-27 20:29:10 +0100

    feat(controller): prevent updating stepper path if encoder doesn't have realtime data

--- src/closedloopcontroller.h
+++ src/closedloopcontroller.h
@@ -10,7 +10,7 @@ class ClosedLoopController
 public:
     TMC5160Controller &stepper;
     Encoder &encoder;
-    uint32_t maxPollInterval = 100;
+    uint32_t maxPollInterval = 50;
     bool enabled;
     double targetAngle;
     double tolerance = 0.1;
@@ -19,7 +19,7 @@ public:
         targetAngle = angle;
         double curAngle = encoder.getAngle();
         double toGo = stepper.mod(targetAngle - curAngle + 180., 360.) - 180.;
-        if (abs(toGo) > tolerance) stepper.moveR(toGo);
+        if (abs(toGo) > tolerance && encoder.hasNewData()) stepper.moveR(toGo);
     }
     double getAngle(){
         return encoder.getAngle();
