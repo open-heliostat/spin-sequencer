dc0ccdfbd68e07c0578740e28f81b3664da29b93
leokeba <leokeba@gmail.com>
2025-02-20 00:39:57 +0100

    feat: add SpinSequencerService to main and remove unused classes

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,8 @@
  **/
 
 #include <ESP32SvelteKit.h>
-#include <LightMqttSettingsService.h>
-#include <LightStateService.h>
 #include <PsychicHttpServer.h>
-#include <StepperControlService.h>
-#include <GPSService.h>
-#include <EncoderService.h>
-#include <ClosedLoopControllerService.h>
-#include <HeliostatService.h>
+#include <SpinSequencerService.h>
 
 #define SERIAL_BAUD_RATE 115200
 
@@ -29,72 +23,20 @@ PsychicHttpServer server;
 ESP32SvelteKit esp32sveltekit(&server, 120);
 
 FastAccelStepperEngine engine = FastAccelStepperEngine();
-LightMqttSettingsService lightMqttSettingsService = LightMqttSettingsService(&server,
-                                                                             &esp32sveltekit);
 
-TMC5160Stepper driver1(10, R_SENSE, 13, 11, 12);
-TMC5160Stepper driver2(7, R_SENSE, 13, 11, 12);
+TMC5160Stepper driver1(D3, R_SENSE, D10, D9, D8);
 
-TMC5160Controller stepper1 = {driver1, engine, 9, 8};
-TMC5160Controller stepper2 = {driver2, engine, 6, 5};
+TMC5160Controller stepper1 = {driver1, engine, D1, D0};
 
-Encoder encoder1 = Encoder(2, 1);
-Encoder encoder2 = Encoder(4, 3, Wire1);
+Encoder encoder1 = Encoder(D4, D5);
 
 ClosedLoopController closedLoopController1 = {stepper1, encoder1};
-ClosedLoopController closedLoopController2 = {stepper2, encoder2};
 
-SerialGPS gpsneo = SerialGPS(Serial1, TX, RX);
+SpinSequencerController spinSequencer = SpinSequencerController{closedLoopController1};
 
-HeliostatController heliostatController = {closedLoopController1, closedLoopController2, gpsneo};
-
-HeliostatService heliostatService = HeliostatService(
-    &server,
-    esp32sveltekit.getSocket(),
-    esp32sveltekit.getFS(),
-    esp32sveltekit.getSecurityManager(),
-    heliostatController);
-
-GPSSettingsService gpsSettingsService = GPSSettingsService(
-    &server,
-    esp32sveltekit.getFS(),
-    esp32sveltekit.getSecurityManager(),
-    &gpsneo);
-
-GPSStateService gpsStateService =  GPSStateService(
-    esp32sveltekit.getSocket(),
-    &gpsSettingsService,
-    &gpsneo,
-    esp32sveltekit.getFeatureService());
-
-// std::vector<TMC5160Controller*> steppers = {&stepper1, &stepper2};
-// std::vector<ClosedLoopController*> closedLoopControllers = {&closedLoopController1, &closedLoopController2};
-
-// StepperSettingsService stepperSettingsService = StepperSettingsService(
-//     esp32sveltekit.getSocket(),
-//     esp32sveltekit.getFS(),
-//     steppers);
-
-// StepperControlService stepperControlService = StepperControlService(
-//     esp32sveltekit.getSocket(),
-//     &stepperSettingsService,
-//     steppers,
-//     esp32sveltekit.getFeatureService());
-
-// EncoderStateService encoderService = EncoderStateService(
-//     esp32sveltekit.getSocket(),
-//     &encoder1);
-
-// ClosedLoopControllerService closedLoopControllerService = ClosedLoopControllerService(
-//     &server,
-//     esp32sveltekit.getSocket(),
-//     esp32sveltekit.getFS(),
-//     esp32sveltekit.getSecurityManager(),
-//     closedLoopController1);
-LightStateService lightStateService = LightStateService(&server,
-                                                        &esp32sveltekit,
-                                                        &lightMqttSettingsService);
-
+SpinSequencerService spinSequencerService = SpinSequencerService{&server, 
+                                                                 &esp32sveltekit, 
+                                                                 spinSequencer};
 void setup()
 {
     // start serial and filesystem
@@ -106,22 +48,9 @@ void setup()
     // start ESP32-SvelteKit
     esp32sveltekit.begin();
 
-    // // load the initial light settings
-    // lightStateService.begin();
-    // // start the light service
-    // lightMqttSettingsService.begin();
-
     engine.init();
     stepper1.init();
-    stepper2.init();
-
-    gpsneo.init();
-    gpsSettingsService.begin();
-    gpsStateService.begin();
-
-    heliostatService.begin();
-    
-    // closedLoopControllerService.begin();
+    spinSequencerService.begin();
 }
 
 unsigned long lastTick = 0;
@@ -130,18 +59,15 @@ void loop()
 {
     // Delete Arduino loop task, as it is not needed in this example
     // vTaskDelete(NULL);
-    heliostatService.loop();
+    spinSequencerService.loop();
     unsigned long now = millis();
-    if (now - lastTick > 1000) {
-        lastTick = now;
-        gpsStateService.loop();
-        if (WiFi.status() == WL_CONNECTED) {
-            lightStateService.updateState(LightState{true, 0, 0.2, 0.1});
-        }
-        else {
-            lightStateService.updateState(LightState{true, 0.2, 0.1, 0});
-        }
-        // if (encoder1.hasNewData()) Serial.println(encoder1.angle);
-        // if (encoder2.hasNewData()) Serial.println(encoder2.angle);
-    }
+    // if (now - lastTick > 1000) {
+    //     lastTick = now;
+    //     if (WiFi.status() == WL_CONNECTED) {
+    //         lightStateService.updateState(LightState{true, 0, 0.2, 0.1});
+    //     }
+    //     else {
+    //         lightStateService.updateState(LightState{true, 0.2, 0.1, 0});
+    //     }
+    // }
 }
