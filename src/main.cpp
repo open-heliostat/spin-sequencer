2ea032559033eda2b10bdb03ba0b71431a283f0f
leokeba <leokeba@gmail.com>
2025-04-14 14:44:01 +0200

    WIP: CAN bus implementation

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 #include <PsychicHttpServer.h>
 #include <SpinSequencerService.h>
 #include <controller.h>
+#include <CAN.h>
+#include <ESP32-TWAI-CAN.hpp>
 
 #define SERIAL_BAUD_RATE 115200
 
@@ -42,7 +44,7 @@ SpinSequencerService spinSequencerService = SpinSequencerService{&server,
 void setup()
 {
     // start serial and filesystem
-    Serial.begin(SERIAL_BAUD_RATE);
+    // Serial.begin(SERIAL_BAUD_RATE);
 
     // increase httpd stack for HttpJsonRouter
     server.config.stack_size = 8192;
@@ -53,7 +55,96 @@ void setup()
     engine.init();
     stepper1.init();
     spinSequencerService.begin();
+
+    // CAN.setPins(D7, D6);
+
+    // // start the CAN bus at 500 kbps
+    // if (!CAN.begin(500E3)) {
+    //     ESP_LOGI("CAN", "Starting CAN failed!");
+    //     while (1);
+    // }
+    // else {
+    //     ESP_LOGI("CAN", "CAN started");
+    // }
+    if(ESP32Can.begin(ESP32Can.convertSpeed(500), D6, D7, 5, 5)) {
+        ESP_LOGI("CAN", "CAN bus started!");
+    } else {
+        ESP_LOGI("CAN", "CAN bus failed!");
+    }
 }
+
+// void receiveCanPacket() {
+//     // try to parse packet
+//     int packetSize = CAN.parsePacket();
+  
+//     if (packetSize || CAN.packetId() != -1) {
+//         // received a packet
+//         Serial.print("Received ");
+    
+//         if (CAN.packetExtended()) {
+//             Serial.print("extended ");
+//         }
+    
+//         if (CAN.packetRtr()) {
+//             // Remote transmission request, packet contains no data
+//             Serial.print("RTR ");
+//         }
+    
+//         Serial.print("packet with id 0x");
+//         Serial.print(CAN.packetId(), HEX);
+    
+//         if (CAN.packetRtr()) {
+//             Serial.print(" and requested length ");
+//             ESP_LOGI("CAN", CAN.packetDlc());
+//         } else {
+//             Serial.print(" and length ");
+//             ESP_LOGI("CAN", packetSize);
+    
+//             // only print packet data for non-RTR packets
+//             while (CAN.available()) {
+//             Serial.print((char)CAN.read());
+//             }
+//             ESP_LOGI("CAN", );
+//         }
+    
+//         ESP_LOGI("CAN", );
+//     }
+// }
+
+void sendCanPacket() {
+    // send packet: id is 11 bits, packet can contain up to 8 bytes of data
+    ESP_LOGI("CAN", "Sending packet ... ");
+  
+    CAN.beginPacket(0x12);
+    CAN.write('h');
+    CAN.write('e');
+    CAN.write('l');
+    CAN.write('l');
+    CAN.write('o');
+    CAN.endPacket();
+  
+    ESP_LOGI("CAN", "Packet sent");
+}
+
+CanFrame rxFrame;
+
+void sendObdFrame(uint8_t obdId) {
+	CanFrame obdFrame = { 0 };
+	obdFrame.identifier = 0x7DF; // Default OBD2 address;
+	obdFrame.extd = 0;
+	obdFrame.data_length_code = 8;
+	obdFrame.data[0] = 2;
+	obdFrame.data[1] = 1;
+	obdFrame.data[2] = obdId;
+	obdFrame.data[3] = 0xAA;    // Best to use 0xAA (0b10101010) instead of 0
+	obdFrame.data[4] = 0xAA;    // CAN works better this way as it needs
+	obdFrame.data[5] = 0xAA;    // to avoid bit-stuffing
+	obdFrame.data[6] = 0xAA;
+	obdFrame.data[7] = 0xAA;
+    // Accepts both pointers and references 
+    ESP32Can.writeFrame(obdFrame);  // timeout defaults to 1 ms
+}
+
 
 unsigned long lastTick = 0;
 
@@ -63,13 +154,24 @@ void loop()
     // vTaskDelete(NULL);
     spinSequencerService.loop();
     unsigned long now = millis();
-    // if (now - lastTick > 1000) {
-    //     lastTick = now;
+    if(ESP32Can.readFrame(rxFrame, 1000)) {
+        // Comment out if too many requests 
+        Serial.printf("Received frame: %03X \r\n", rxFrame.identifier);
+        if(rxFrame.identifier == 0x7E8) {   // Standard OBD2 frame responce ID
+            Serial.printf("Collant temp: %3d°C \r\n", rxFrame.data[3] - 40); // Convert to °C
+        }
+    }
+    // receiveCanPacket();
+    if (now - lastTick > 1000) {
+        ESP_LOGI("CAN", "Sending packet ... ");
+        lastTick = now;
+        // sendCanPacket();
+        sendObdFrame(5); // For coolant temperature
     //     if (WiFi.status() == WL_CONNECTED) {
     //         lightStateService.updateState(LightState{true, 0, 0.2, 0.1});
     //     }
     //     else {
     //         lightStateService.updateState(LightState{true, 0.2, 0.1, 0});
     //     }
-    // }
+    }
 }
--- src/sun.h
+++
@@ -1,8 +0,0 @@
-#ifndef SUNTRACKER
-#define SUNTRACKER
-#include <SolarPosition.h>
-#include "TimeLib.h"
-#include "geometry.h"
-SphericalCoordinate computeSolarPosition(double latitude, double longitude);
-void setupSolarTracker();
-#endif
