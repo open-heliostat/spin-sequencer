ce4050d3f6276514f6a37768af4083ed9743323e
Léo Baqué <leokeba@gmail.com>
2025-05-08 18:00:52 +0200

    feat(rest): add options parameter to GET and POST requests
    Allow passing additional fetch options to getJsonRest and postJsonRest
    functions. This enables customization of requests, such as adding headers
    or credentials, improving flexibility for different API requirements.

--- interface/src/lib/stores/rest.ts
+++ interface/src/lib/stores/rest.ts
@@ -1,10 +1,11 @@
-export async function getJsonRest<T>(path: string, data : T) {
+export async function getJsonRest<T>(path: string, data : T, options: any = {}) {
     try {
         const response = await fetch(path, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
-            }
+            },
+            ...options
         });
         let json = await response.json();
         data = json;
@@ -14,13 +15,14 @@ export async function getJsonRest<T>(path: string, data : T) {
     return data;
 }
 
-export async function postJsonRest<T>(path: string, data: T) {
+export async function postJsonRest<T>(path: string, data: T, options: any = {}) {
     try {
         const response = await fetch(path, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
+            ...options,
             body: JSON.stringify(data)
         });
         if (response.status == 200) {
