db75e99dc9d8e1bbea53637c70eb68b9c4ba01dc
leokeba <leokeba@gmail.com>
2025-05-11 23:56:43 +0200

    fix(vite): Disable viteLittleFS plugin to fix cache invalidation issues
    The doc says ESP_LittleFS has a 32 char filename limit but after some research it seems that it's actually 64 on ESP32 platforms, so the tradeoff seems worth it.

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -10,18 +10,18 @@ const config: UserConfig = {
 			compiler: 'svelte'
 		}),
 		// Shorten file names for LittleFS 32 char limit
-		viteLittleFS()
+		// viteLittleFS()
 	],
 	server: {
 		proxy: {
 			// Proxying REST: http://localhost:5173/rest/bar -> http://192.168.1.83/rest/bar
 			'/rest': {
-				target: 'http://172.20.10.8',
+				target: 'http://192.168.1.202/',
 				changeOrigin: true
 			},
 			// Proxying websockets ws://localhost:5173/ws -> ws://192.168.1.83/ws
 			'/ws': {
-				target: 'ws://172.20.10.8',
+				target: 'ws://192.168.1.202/',
 				changeOrigin: true,
 				ws: true
 			}
